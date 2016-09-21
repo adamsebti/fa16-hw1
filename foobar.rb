11f87bdf1faab5074c61f7672d2c17b095db6e46
Sigma Tech25 <sigma25@Dinos-Retina.local>
2016-09-21 15:13:31 -0700

    hw1
    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a = a.map! { |e| e = e.to_i }
+    a = a.map! { |e| e += 2 }
+    a = a.uniq
+    a = a.delete_if &:odd?
+    a = a.delete_if {|x| x > 10 }
+    a.reduce(0, :+)
   end
 end
 
